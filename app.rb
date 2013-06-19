9f674006f06c765a74037da9e065193816c0081a
Simon Orr <simon@simonorrstudio.com>
2013-06-19 03:36:50 -0700

    made the etag have some sort of meaning

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require 'sinatra'
 require 'fortune_gem'
 
 get '/edition/' do
-  @fortune = FortuneGem.give_fortune({:max_length => 220})
-  etag Digest::MD5.hexdigest(Time.now.utc.strftime('%l%p, %d %b %M %S %Y %Z'))  
+  @fortune = FortuneGem.give_fortune({:max_length => 225})
+  etag Digest::MD5.hexdigest(@fortune + Time.now.utc.strftime('%l%p, %d %b %M %S %Y %Z'))  
 
   erb :edition
 end
