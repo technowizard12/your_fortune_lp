e5e7b8f26331eb2dba7c55d18c7f443e2ea10a6e
Simon Orr <simon@simonorrstudio.com>
2013-06-19 02:29:48 -0700

    instituted length limit of 220

--- app.rb
+++ app.rb
@@ -2,13 +2,13 @@ require 'sinatra'
 require 'fortune_gem'
 
 get '/edition/' do
-  @fortune = FortuneGem.give_fortune
+  @fortune = FortuneGem.give_fortune({:max_length => 220})
   etag Digest::MD5.hexdigest(Time.now.utc.strftime('%l%p, %d %b %M %S %Y %Z'))  
 
   erb :edition
 end
 
 get '/sample/' do
-  @fortune = FortuneGem.give_fortune
+  @fortune = FortuneGem.give_fortune({:max_length => 220})
   erb :edition
 end
