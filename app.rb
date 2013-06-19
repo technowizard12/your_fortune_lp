c7b91129412c776710c6bb73ee24b29043519784
Simon Orr <simon@simonorrstudio.com>
2013-06-19 02:02:28 -0700

    fixed etag problem

--- app.rb
+++ app.rb
@@ -8,5 +8,7 @@ end
 
 get '/sample/' do
   @fortune = FortuneGem.give_fortune({:max_length => 200})
+  @random = rand(100)
+  etag Digest::MD5.hexdigest(@random+Time.now.utc.strftime('%l%p, %d %b %Y %Z'))  
   erb :edition
 end
