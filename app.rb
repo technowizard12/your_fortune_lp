0c23f633c069d697dbd5d647049d001e9a34cb18
Simon Orr <simon@simonorrstudio.com>
2013-06-19 03:40:36 -0700

    changed to same hour or whatever

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require 'fortune_gem'
 
 get '/edition/' do
   @fortune = FortuneGem.give_fortune({:max_length => 250})
-  etag Digest::MD5.hexdigest(@fortune + Time.now.utc.strftime('%l%p, %d %b %M %Y %Z'))  
+  etag Digest::MD5.hexdigest(@fortune + Time.now.utc.strftime('%l%p, %d %b %Y %Z'))  
 
   erb :edition
 end
