5790f610b5af61be5b858e875355aac3374380ad
Simon Orr <simon@simonorrstudio.com>
2013-06-19 03:37:50 -0700

    fuck that. twice in the same minute

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require 'fortune_gem'
 
 get '/edition/' do
   @fortune = FortuneGem.give_fortune({:max_length => 225x})
-  etag Digest::MD5.hexdigest(@fortune + Time.now.utc.strftime('%l%p, %d %b %Y %Z'))  
+  etag Digest::MD5.hexdigest(@fortune + Time.now.utc.strftime('%l%p, %d %b %M %Y %Z'))  
 
   erb :edition
 end
