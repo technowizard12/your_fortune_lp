e304058dce54b4ba0991156463b915e6dc030b74
Simon Orr <sorr@willamette.edu>
2013-07-29 14:50:27 -0700

    added bottom padding, fixed meta.json delivered every

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require 'fortune_gem'
 
 get '/edition/' do
-  @fortune = FortuneGem.give_fortune({:max_length => 250})
+  @fortune = FortuneGem.give_fortune({:max_length => 235})
   etag Digest::MD5.hexdigest(@fortune + Time.now.utc.strftime('%l%p, %d %b %Y %Z'))  
 
   #etag is the same if the same fortune is accessed within a minute
